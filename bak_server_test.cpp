4fe8d3b6a4420fcc435a05224baa0e7da96524b8
youahentai <z1654026163@163.com>
2020-07-08 21:16:02 +0800

    2

--- bak_server_test.cpp
+++ bak_server_test.cpp
@@ -60,6 +60,7 @@ int main(int argc,char** argv)
         ret=recv(connfd,buffer,BUF_SIZE-1,0);
         rec=buffer;
         cout<<ret<<" "<<rec<<endl;*/
+        sleep(2);
 
         memset(buffer,0,BUF_SIZE);
         ret=recv(connfd,buffer,BUF_SIZE-1,MSG_OOB);
